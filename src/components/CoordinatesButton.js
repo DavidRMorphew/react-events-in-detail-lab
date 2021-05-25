c360f31905ffc62d528d7a77d79f10184be18c38
DavidRMorphew <morphewd@umich.edu>
2021-05-25 19:00:57 -0400

    commit tree c6fbc53a41d8d119321730a3f9210f857cf88d9f

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+class CoordinatesButton extends Component {
+    
+    handleClick = (e) => {
+        const coordinates = [e.clientX, e.clientY]
+        this.props.onReceiveCoordinates(coordinates)
+    }
+    
+    render(){
+        return(
+            <div>
+                <button onClick={this.handleClick}>Click Me!</button>
+            </div>
+        )
+    }
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+
+import React, { Component } from 'react'
+class DelayedButton extends Component {
+    
+    persistEventForDelay = (e) => {
+        e.persist()
+        setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
+    }
+    
+    render(){
+        return(
+            <div>
+                <button onClick={this.persistEventForDelay}>Click to Delay Me!</button>
+            </div>
+        )
+    }
+}
+
+export default DelayedButton
